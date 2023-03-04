ac3373cc80b0caf3542acd677ae7a0b9faf5b5e7
kaoutar <126948217+RAVEN232@users.noreply.github.com>
2023-03-04 11:31:20 +0100

    message

---
+++ PROJET.html/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#cup").mouseenter(function(){
+        $(this).attr("src","anim2.png");
+    });
+    $("#cup").mouseleave(function(){
+        $(this).attr("src","anim1.png");
+    }); 
+});
+
+
